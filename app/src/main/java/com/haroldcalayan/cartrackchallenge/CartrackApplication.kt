52a789a1a29a58787cc4d9b4b3f5334d85534e9c
Harold Calayan <haroldcalayan@gmail.com>
2020-08-10 05:42:47 +0800

    Initialize dagger component

--- app/src/main/java/com/haroldcalayan/cartrackchallenge/CartrackApplication.kt
+++ app/src/main/java/com/haroldcalayan/cartrackchallenge/CartrackApplication.kt
@@ -8,14 +8,24 @@
 package com.haroldcalayan.cartrackchallenge
 
 import android.app.Application
+import com.haroldcalayan.cartrackchallenge.di.DaggerAppComponent
+import com.haroldcalayan.cartrackchallenge.di.component.AppComponent
 import timber.log.Timber
 
 class CartrackApplication : Application() {
+
+    val appComponent: AppComponent by lazy {
+        initializeComponent()
+    }
 
     override fun onCreate() {
         super.onCreate()
         instance = this
         initLog()
+    }
+
+    open fun initializeComponent(): AppComponent {
+        return DaggerAppComponent.factory().create(applicationContext)
     }
 
     private fun initLog() {
