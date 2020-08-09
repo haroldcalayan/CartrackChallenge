21bd43d0d5472b64040d1d6fcf4538092d7d3a6e
Harold Calayan <haroldcalayan@gmail.com>
2020-08-10 05:39:19 +0800

    Add repository files

---
+++ app/src/main/java/com/haroldcalayan/cartrackchallenge/data/UserRepository.kt
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2020, Cartrack Challenge. All rights reserved.
+ *
+ * Created by Harold Calayan on 8/10/2020
+ *
+ */
+
+package com.haroldcalayan.cartrackchallenge.data
+
+class UserRepository {
+}
