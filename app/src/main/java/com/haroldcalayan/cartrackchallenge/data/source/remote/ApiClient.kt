75e77c11c04c0297281709b0f1685150acffe1ba
Harold Calayan <haroldcalayan@gmail.com>
2020-08-10 05:41:23 +0800

    Add retrofit service and client

---
+++ app/src/main/java/com/haroldcalayan/cartrackchallenge/data/source/remote/ApiClient.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2020, Cartrack Challenge. All rights reserved.
+ *
+ * Created by Harold Calayan on 8/10/2020
+ *
+ */
+
+package com.haroldcalayan.cartrackchallenge.data.source.remote
+
+import com.github.leonardoxh.livedatacalladapter.LiveDataCallAdapterFactory
+import com.github.leonardoxh.livedatacalladapter.LiveDataResponseBodyConverterFactory
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiClient(
+    private val baseURL: String,
+    private val okHttpBuilder: OkHttpClient.Builder) {
+
+    private lateinit var apiService: ApiService
+
+    init {
+        createClient(baseURL)
+    }
+
+    fun createClient(baseUrl: String) {
+        val retrofit = Retrofit.Builder()
+            .client(okHttpBuilder.build())
+            .addCallAdapterFactory(LiveDataCallAdapterFactory.create())
+            .addConverterFactory(LiveDataResponseBodyConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(baseUrl)
+            .build()
+
+        apiService = retrofit.create(ApiService::class.java)
+    }
+
+    fun getService(): ApiService? {
+        return apiService
+    }
+}
