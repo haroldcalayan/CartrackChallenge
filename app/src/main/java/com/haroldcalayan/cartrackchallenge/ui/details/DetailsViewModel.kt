a26a8f471001484fad198c88e60ca4d4a93ca544
Harold Calayan <haroldcalayan@gmail.com>
2020-08-10 05:43:46 +0800

    Add viewmodels

---
+++ app/src/main/java/com/haroldcalayan/cartrackchallenge/ui/details/DetailsViewModel.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2020, Cartrack Challenge. All rights reserved.
+ *
+ * Created by Harold Calayan on 8/10/2020
+ *
+ */
+
+package com.haroldcalayan.cartrackchallenge.ui.details
+
+import com.haroldcalayan.cartrackchallenge.base.BaseViewModel
+
+class DetailsViewModel : BaseViewModel() {
+}
---
+++ app/src/main/java/com/haroldcalayan/cartrackchallenge/ui/splash/SplashViewModel.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020, Cartrack Challenge. All rights reserved.
+ *
+ * Created by Harold Calayan on 8/10/2020
+ *
+ */
+
+package com.haroldcalayan.cartrackchallenge.ui.splash
+
+import com.haroldcalayan.cartrackchallenge.base.BaseViewModel
+
+class SplashViewModel : BaseViewModel()
