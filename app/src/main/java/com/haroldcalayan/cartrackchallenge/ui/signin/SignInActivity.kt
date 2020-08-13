de5e3046f98778f9fe5a4d3fcba5ddf1ecb6ca8e
Harold Calayan <haroldcalayan@gmail.com>
2020-08-14 00:29:59 +0800

    Implement API in login and display proper messages

--- app/src/main/java/com/haroldcalayan/cartrackchallenge/ui/signin/SignInActivity.kt
+++ app/src/main/java/com/haroldcalayan/cartrackchallenge/ui/signin/SignInActivity.kt
@@ -7,18 +7,35 @@
 
 package com.haroldcalayan.cartrackchallenge.ui.signin
 
+import android.app.Activity
+import androidx.lifecycle.Observer
 import com.haroldcalayan.cartrackchallenge.R
 import com.haroldcalayan.cartrackchallenge.base.BaseActivity
 import com.haroldcalayan.cartrackchallenge.databinding.ActivitySigninBinding
-import kotlinx.android.synthetic.main.activity_signin.*
 
 class SignInActivity : BaseActivity<ActivitySigninBinding, SignInViewModel>() {
 
     override fun getLayout() = R.layout.activity_signin
 
-    override fun initViews() {
-        super.initViews()
+    override fun subscribe() {
+        super.subscribe()
 
-        textinputedittext_signin_username.text
+        getViewModel().loading.observe(this, Observer {
+
+        })
+
+        getViewModel().loginSuccess.observe(this, Observer {
+            setResult(Activity.RESULT_OK)
+            finish()
+        })
+
+        getViewModel().errorMessage.observe(this, Observer {
+            showToastMessage(it)
+        })
+
+        getViewModel().close.observe(this, Observer {
+            setResult(Activity.RESULT_CANCELED)
+            finish()
+        })
     }
 }
--- app/src/main/java/com/haroldcalayan/cartrackchallenge/ui/signin/SignInViewModel.kt
+++ app/src/main/java/com/haroldcalayan/cartrackchallenge/ui/signin/SignInViewModel.kt
@@ -10,18 +10,28 @@ package com.haroldcalayan.cartrackchallenge.ui.signin
 import android.text.TextUtils
 import androidx.databinding.ObservableField
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.haroldcalayan.cartrackchallenge.R
 import com.haroldcalayan.cartrackchallenge.base.BaseViewModel
+import com.haroldcalayan.cartrackchallenge.data.source.AuthenticationRepository
 import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
 
 class SignInViewModel : BaseViewModel() {
+
+    @Inject
+    lateinit var authenticationRepository: AuthenticationRepository
 
     val username = ObservableField("")
     val password = ObservableField("")
 
-    fun onClickClose() {
+    private val _close = MutableLiveData<Unit>()
+    private val _loginSuccess = MutableLiveData<Unit>()
 
+    fun onClickClose() {
+        _close.value = Unit
     }
 
     fun onClickSubmit() {
@@ -29,18 +39,36 @@ class SignInViewModel : BaseViewModel() {
         val passwordStr = password.get()
 
         if (TextUtils.isEmpty(usernameStr)) {
-            _errorMessage.value = R.string.app_name
+            _errorMessage.value = R.string.signin_error_username_must_not_empty
             return
         }
 
         if (TextUtils.isEmpty(passwordStr)) {
-            _errorMessage.value = R.string.app_name
+            _errorMessage.value = R.string.signin_error_password_must_not_empty
+            return
+        }
+
+        if (usernameStr!!.length < MINIMUM_USERNAME_LENGTH) {
+            _errorMessage.value = R.string.signin_error_username_must_3_characters
+            return
+        }
+
+        if (passwordStr!!.length < MINIMUM_PASSWORD_LENGTH) {
+            _errorMessage.value = R.string.signin_error_password_must_8_characters
             return
         }
 
         _loading.value = true
-
         viewModelScope.launch {
+            val account = authenticationRepository.getAccount(usernameStr)
+            Timber.d("account: $account")
+            if(account == null) {
+                _errorMessage.value = R.string.signin_error_user_is_not_registered
+            } else if(account.password != passwordStr) {
+                _errorMessage.value = R.string.signin_error_password_does_not_match
+            } else {
+                _loginSuccess.value = Unit
+            }
 
             _loading.value = false
         }
@@ -49,10 +77,13 @@ class SignInViewModel : BaseViewModel() {
     /*
     LiveData Getters
      */
-    val loading: LiveData<Boolean>
-        get() = _loading
-    val loginSuccess: LiveData<Int>
+    val close: LiveData<Unit>
+        get() = _close
+    val loginSuccess: LiveData<Unit>
         get() = _loginSuccess
-    val errorMessage: LiveData<Int>
-        get() = _errorMessage
+
+    companion object {
+        const val MINIMUM_USERNAME_LENGTH = 3
+        const val MINIMUM_PASSWORD_LENGTH = 8
+    }
 }
