f4298b193134d83c41bb3eb0e015614bda7567d4
Harold Calayan <haroldcalayan@gmail.com>
2020-08-14 05:30:38 +0800

    Add details screen

--- app/src/main/java/com/haroldcalayan/cartrackchallenge/ui/details/DetailsActivity.kt
+++ app/src/main/java/com/haroldcalayan/cartrackchallenge/ui/details/DetailsActivity.kt
@@ -1,18 +1,110 @@
 /*
  * Copyright (c) 2020, Cartrack Challenge. All rights reserved.
  *
- * Created by Harold Calayan on 8/10/2020
+ * Created by Harold Calayan on 8/14/2020
  *
  */
 
 package com.haroldcalayan.cartrackchallenge.ui.details
 
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.gson.Gson
 import com.haroldcalayan.cartrackchallenge.R
 import com.haroldcalayan.cartrackchallenge.base.BaseActivity
+import com.haroldcalayan.cartrackchallenge.data.model.User
 import com.haroldcalayan.cartrackchallenge.databinding.ActivityDetailsBinding
+import kotlinx.android.synthetic.main.activity_details.*
+import kotlinx.android.synthetic.main.content_scrolling.*
+import java.lang.StringBuilder
+
 
 class DetailsActivity : BaseActivity<ActivityDetailsBinding, DetailsViewModel>() {
 
+    private val gson = Gson()
+    private lateinit var user: User
+
     override fun getLayout() = R.layout.activity_details
 
+    override fun initViews() {
+        super.initViews()
+        initToolbar()
+    }
+
+    override fun initData() {
+        super.initData()
+        user = gson.fromJson(intent.getStringExtra(EXTRA_USER), User::class.java)
+        updateUI()
+    }
+
+    override fun subscribe() {
+        super.subscribe()
+        getViewModel().fab.observe(this, Observer {
+            showSnackBarMessage(fab, R.string.details_warning_feature_is_not_yet_implemented)
+        })
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+
+    private fun updateUI() {
+        toolbar_layout.title = user.name
+        textview_details_basic_info.text = StringBuilder()
+            .append("Username: ")
+            .append(user.username)
+            .append("\nWebsite: ")
+            .append(user.website)
+            .toString()
+        textview_details_address.text = StringBuilder()
+            .append("Address: ")
+            .append(user.address.suite)
+            .append(" ")
+            .append(user.address.street)
+            .append(" St. ")
+            .append(user.address.city)
+            .append(" ")
+            .append(user.address.zipcode)
+            .append("\nGeo: ")
+            .append(user.address.geo.lat)
+            .append(" lat ")
+            .append(user.address.geo.lng)
+            .append(" lng")
+            .toString()
+        textview_details_contacts.text = StringBuilder()
+            .append("Phone: ")
+            .append(user.phone)
+            .append("\nEmail: ")
+            .append(user.email)
+            .toString()
+        textview_details_work.text = StringBuilder()
+            .append("Company Name: ")
+            .append(user.company.name)
+            .append("\nCatch Phrase: ")
+            .append(user.company.catchPhrase)
+            .append("\nBS: ")
+            .append(user.company.bs)
+            .toString()
+    }
+
+    companion object {
+        const val EXTRA_USER = "user"
+    }
 }
--- app/src/main/java/com/haroldcalayan/cartrackchallenge/ui/details/DetailsViewModel.kt
+++ app/src/main/java/com/haroldcalayan/cartrackchallenge/ui/details/DetailsViewModel.kt
@@ -1,13 +1,27 @@
 /*
  * Copyright (c) 2020, Cartrack Challenge. All rights reserved.
  *
- * Created by Harold Calayan on 8/10/2020
+ * Created by Harold Calayan on 8/14/2020
  *
  */
 
 package com.haroldcalayan.cartrackchallenge.ui.details
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.haroldcalayan.cartrackchallenge.base.BaseViewModel
 
 class DetailsViewModel : BaseViewModel() {
+
+    private val _fab = MutableLiveData<Unit>()
+
+    fun onFabClose() {
+        _fab.value = Unit
+    }
+
+    /*
+    LiveData Getters
+     */
+    val fab: LiveData<Unit>
+        get() = _fab
 }
